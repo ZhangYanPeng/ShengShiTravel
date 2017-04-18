b860699657562f36cc048cc456199fccfe371254
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-04-18 15:17:06 +0800

    Merge remote-tracking branch 'refs/remotes/origin/Ming' into Zhang
    # Conflicts:
    #	src/main/webapp/dist/www/js/my-app.js
    #	target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    
    #	target/m2e-wtp/web-resources/META-INF/maven/com.shengshi/travel/pom.properties

--- src/main/webapp/dist/www/js/my-app.js
+++ src/main/webapp/dist/www/js/my-app.js
@@ -24,7 +24,7 @@ var pageNo = '0';
 // index页启动时加载listTab
 showListTab();
 //me页面
-showPersonalInfo(){
+showPersonalInfo();
 
 
 // 底部TAB点击
@@ -315,99 +315,6 @@ function getListInfos(type,order,page) {
     });
 }
 
-function showPersonalInfo(){
-	//personal information load
-			$$.ajax({
-				async : false,
-				cache : false,
-				type : 'POST',
-				crossDomain : true,
-				dataType : 'json',
-				data : {
-					'openid' : 'OPENID',
-				},
-				url : "http://localhost:8080/travel/customer/view",
-				error : function(data) {// 请求失败处理函数
-					alert("获取数据失败！");
-				},
-				success : function(data) {
-					var info = data;
-					$$("#headimgurl").attr("src",info.headimgurl);
-					$$("#nickname").html(info.nickname);
-					$$("#sex").html(parseSex(info.sex));
-					$$("#address").html(info.province+","+info.city);
-					$$("#name").html(info.name);
-					$$("#phone").html(info.phone);
-					
-					function init_personal(){
-						myApp.popup('.popup-personal-info');
-						$$("#li_nickname").hide();
-						$$("#li_sex").hide();
-						$$("#li_address").hide();
-						$$("#li_name").hide();
-						$$("#li_phone").hide();
-						$$("#openid_edit").val(info.openid);
-						$$("#headimgurl_edit").val(info.headimgurl);
-						$$("#nickname_edit").val(info.nickname);
-						$$("#sex_edit").val(info.sex);
-						$$("#country_edit").val(info.country);
-						$$("#province_edit").val(info.province);
-						$$("#city_edit").val(info.city);
-						$$("#name_edit").val(info.name);
-						$$("#phone_edit").val(info.phone);
-					};
-					
-					$$("#href_nickname").on('click',function(){
-						init_personal();
-						$$("#li_nickname").show();
-					});
-					$$("#href_sex").on('click',function(){
-						init_personal();
-						$$("#li_sex").show();
-					});
-					$$("#href_address").on('click',function(){
-						init_personal();
-						$$("#li_address").show();
-					});
-					$$("#href_name").on('click',function(){
-						init_personal();
-						$$("#li_name").show();
-					});
-					$$("#href_phone").on('click',function(){
-						init_personal();
-						$$("#li_phone").show();
-					});
-					
-					$$('.popup-personal-info').on('close', function () {
-						var storedData = myApp.formToJSON("#personal-form");
-						$$.ajax({
-							async : false,
-							cache : false,
-							type : 'POST',
-							crossDomain : true,
-							dataType : 'json',
-							data : storedData,
-							error : function(data) {// 请求失败处理函数
-								alert("获取数据失败！");
-							},
-							url : "http://localhost:8080/travel/customer/edit",
-							success : function(data) {
-								info = data;
-								$$("#headimgurl").attr("src",data.headimgurl);
-								$$("#nickname").html(data.nickname);
-								$$("#sex").html(parseSex(data.sex));
-								$$("#address").html(data.province+","+data.city);
-								$$("#name").html(data.name);
-								$$("#phone").html(data.phone);
-								init_personal();
-							}
-						});
-					});
-				}
-			});
-}
-
-
 function parseSex(sex){
 	switch(sex){
 	case "0": return "保密";
@@ -415,3 +322,98 @@ function parseSex(sex){
 	case "2": return "女";
 	}
 }
+
+function showPersonalInfo(){
+	//personal information load
+	$$.ajax({
+		async : false,
+		cache : false,
+		type : 'POST',
+		crossDomain : true,
+		dataType : 'json',
+		data : {
+			'openid' : 'OPENID',
+		},
+		url : "http://localhost:8080/travel/customer/view",
+		error : function(data) {// 请求失败处理函数
+			alert("获取数据失败！");
+		},
+		success : function(data) {
+			var info = data;
+			$$("#headimgurl").attr("src",info.headimgurl);
+			$$("#nickname").html(info.nickname);
+			$$("#sex").html(parseSex(info.sex));
+			$$("#address").html(info.province+","+info.city);
+			$$("#name").html(info.name);
+			$$("#phone").html(info.phone);
+			
+			function init_personal(){
+				myApp.popup('.popup-personal-info');
+				$$("#li_nickname").hide();
+				$$("#li_sex").hide();
+				$$("#li_address").hide();
+				$$("#li_name").hide();
+				$$("#li_phone").hide();
+				$$("#openid_edit").val(info.openid);
+				$$("#headimgurl_edit").val(info.headimgurl);
+				$$("#nickname_edit").val(info.nickname);
+				$$("#sex_edit").val(info.sex);
+				$$("#country_edit").val(info.country);
+				$$("#province_edit").val(info.province);
+				$$("#city_edit").val(info.city);
+				$$("#name_edit").val(info.name);
+				$$("#phone_edit").val(info.phone);
+			};
+			
+			$$("#href_nickname").on('click',function(){
+				init_personal();
+				$$("#li_nickname").show();
+			});
+			$$("#href_sex").on('click',function(){
+				init_personal();
+				$$("#li_sex").show();
+			});
+			$$("#href_address").on('click',function(){
+				init_personal();
+				$$("#li_address").show();
+			});
+			$$("#href_name").on('click',function(){
+				init_personal();
+				$$("#li_name").show();
+			});
+			$$("#href_phone").on('click',function(){
+				init_personal();
+				$$("#li_phone").show();
+			});
+			
+			$$('.popup-personal-info').on('close', function () {
+				var storedData = myApp.formToJSON("#personal-form");
+				$$.ajax({
+					async : false,
+					cache : false,
+					type : 'POST',
+					crossDomain : true,
+					dataType : 'json',
+					data : storedData,
+					error : function(data) {// 请求失败处理函数
+						alert("获取数据失败！");
+					},
+					url : "http://localhost:8080/travel/customer/edit",
+					success : function(data) {
+						info = data;
+						$$("#headimgurl").attr("src",data.headimgurl);
+						$$("#nickname").html(data.nickname);
+						$$("#sex").html(parseSex(data.sex));
+						$$("#address").html(data.province+","+data.city);
+						$$("#name").html(data.name);
+						$$("#phone").html(data.phone);
+						init_personal();
+					}
+				});
+			});
+		}
+	});
+}
+
+
+
