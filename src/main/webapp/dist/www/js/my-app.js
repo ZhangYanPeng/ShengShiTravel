74119ad6d62d95d61b1177468c8e1e9f43709019
Meshine001 <mingshuai001@hotmail.com>
2017-04-18 21:28:06 +0800

    基本布局OK

--- src/main/webapp/dist/www/js/my-app.js
+++ src/main/webapp/dist/www/js/my-app.js
@@ -9,7 +9,6 @@ var mainView = myApp.addView('.view-main', {
 
 });
 
-
 var baseUrl = "http://localhost:8080/travel/";
 
 var infoType = '0';// 列表类型
@@ -17,50 +16,47 @@ var orderType = '1';// 排序
 var currentPage = '1';
 var pageNo = '0';
 
-
 /**
  * index.html
  */
 // index页启动时加载listTab
 showListTab();
-//me页面
-showPersonalInfo();
 
 
 // 底部TAB点击
-$$('.tab-link').on('click',function (e) {
-    var tabPage = $$(e.target).parent().prop('id');
-    // console.log(tabPage);
-    if (tabPage == 'publish'){
-        myApp.popup('.popup-publish');
-        return;
-    }
-    $$.get(tabPage+'.html',{},function (data) {
-        $$('#'+tabPage+'-tab').html(data);
-        switch(tabPage){
-        case 'list':
-        	showListTab();
-        break;
-case 'me' :
- showPersonalInfo();
-break;
-        }
-    })
+$$('.tab-link').on('click', function(e) {
+	var tabPage = $$(e.target).parent().prop('id');
+	// console.log(tabPage);
+	if (tabPage == 'publish') {
+		myApp.popup('.popup-publish');
+		return;
+	}
+	$$.get(tabPage + '.html', {}, function(data) {
+		$$('#' + tabPage + '-tab').html(data);
+		switch (tabPage) {
+		case 'list':
+			showListTab();
+			break;
+		case 'me':
+			showPersonalInfo();
+			break;
+		}
+	});
 });
 
 // publish popup form change
-$$('.category').on('change',function (e) {
-    var target = $$(e.target);
-    // console.log(target);
-    if (target.attr('id') == 'cYoushunche') {
-        $$('#form-chexing').show();
-        $$('#form-kongzuoshu').show();
-        $$('#type').val('0');
-    } else {
-        $$('#form-chexing').hide();
-        $$('#form-kongzuoshu').hide();
-        $$('#type').val('1');
-    }
+$$('.category').on('change', function(e) {
+	var target = $$(e.target);
+	// console.log(target);
+	if (target.attr('id') == 'cYoushunche') {
+		$$('#form-chexing').show();
+		$$('#form-kongzuoshu').show();
+		$$('#type').val('0');
+	} else {
+		$$('#form-chexing').hide();
+		$$('#form-kongzuoshu').hide();
+		$$('#type').val('1');
+	}
 });
 
 /**
@@ -72,153 +68,163 @@ var ptrContent = $$('.pull-to-refresh-content');
 /**
  * 下拉刷新绑定
  */
-ptrContent.on('refresh', function (e) {
-    var  target = $$('.toolbar .tab-link').filter(function (index,el) {
-        return $$(this).hasClass('active');
-    });
-    console.log('更新==>'+target.attr('id'));
+ptrContent.on('refresh', function(e) {
+	var target = $$('.toolbar .tab-link').filter(function(index, el) {
+		return $$(this).hasClass('active');
+	});
+	console.log('更新==>' + target.attr('id'));
 
-    switch (target.attr("id")){
-        case 'list':
-        	infoType = '0';
-        	currentPage = '1';
-            $$('.info-list').html('');
-        	getListInfos(infoType,orderType,currentPage);
-            myApp.attachInfiniteScroll($$('.infinite-scroll'));
-            break;
-        case 'lift-list':
-            break;
-        case 'me':
-            break;
-    }
-    myApp.pullToRefreshDone();
+	switch (target.attr("id")) {
+	case 'list':
+		infoType = '0';
+		currentPage = '1';
+		$$('.info-list').html('');
+		getListInfos(infoType, orderType, currentPage);
+		myApp.attachInfiniteScroll($$('.infinite-scroll'));
+		break;
+	case 'lift-list':
+		break;
+	case 'me':
+		break;
+	}
+	myApp.pullToRefreshDone();
 
 });
 
 /**
  * 绑定点击事件
  */
-$$(document).on('click',function (e) {
-    var target = $$(e.target);
-    // console.log(target);
+$$(document)
+		.on(
+				'click',
+				function(e) {
+					var target = $$(e.target);
+					// console.log(target);
 
-    //发布信息
-    if($$(target).hasClass('publish-from-submit')){
-    	publishInfoSubmit();
-    }
+					// 发布信息
+					if ($$(target).hasClass('publish-from-submit')) {
+						publishInfoSubmit();
+					}
 
-    //按发布时间排序
-    if($$(target).hasClass('sort-publish-time')){
+					// 按发布时间排序
+					if ($$(target).hasClass('sort-publish-time')) {
 
-        if (orderType != '1' && orderType !='2'){
-            orderType = '1';
-        }else {
-            orderType = orderType=='1'?'2':'1';
-        }
-        $(target).next('i').attr('class',orderType=='1'?'fa fa-sort-up':'fa fa-sort-down');
-        $$('.info-list').html('');
-        currentPage = '1';
-        getListInfos(infoType,orderType,currentPage);
-    }
+						if (orderType != '1' && orderType != '2') {
+							orderType = '1';
+						} else {
+							orderType = orderType == '1' ? '2' : '1';
+						}
+						$(target).next('i').attr(
+								'class',
+								orderType == '1' ? 'fa fa-sort-up'
+										: 'fa fa-sort-down');
+						$$('.info-list').html('');
+						currentPage = '1';
+						getListInfos(infoType, orderType, currentPage);
+					}
 
-    //按出发时间排序
-    if($$(target).hasClass('sort-start-time')){
-        if (orderType != '3' && orderType !='4'){
-            orderType = '3';
-        }else {
-            orderType = orderType=='3'?'4':'3';
-        }
-        $$('.info-list').html('');
-        $(target).next('i').attr('class',orderType=='3'?'fa fa-sort-up':'fa fa-sort-down');
-        currentPage = '1';
+					// 按出发时间排序
+					if ($$(target).hasClass('sort-start-time')) {
+						if (orderType != '3' && orderType != '4') {
+							orderType = '3';
+						} else {
+							orderType = orderType == '3' ? '4' : '3';
+						}
+						$$('.info-list').html('');
+						$(target).next('i').attr(
+								'class',
+								orderType == '3' ? 'fa fa-sort-up'
+										: 'fa fa-sort-down');
+						currentPage = '1';
 
-        getListInfos(infoType,orderType,currentPage);
-    }
+						getListInfos(infoType, orderType, currentPage);
+					}
 
-    //选择地点查询 提交
-    if($$(target).hasClass('choose-location-submit')){
-        var s = $$('.choose-location-s').val();
-        var d = $$('.choose-location-d').val();
-        console.log(s+","+d);
-        $$('.info-list').html('');
-        currentPage = '1';
-        getListInfos(infoType,orderType,currentPage,s,d);
-        myApp.closeModal('.popover-choose-location');
-    }
+					// 选择地点查询 提交
+					if ($$(target).hasClass('choose-location-submit')) {
+						var s = $$('.choose-location-s').val();
+						var d = $$('.choose-location-d').val();
+						console.log(s + "," + d);
+						$$('.info-list').html('');
+						currentPage = '1';
+						getListInfos(infoType, orderType, currentPage, s, d);
+						myApp.closeModal('.popover-choose-location');
+					}
 
-
-});
+				});
 
 /**
  * 发布信息
+ * 
  * @returns
  */
-function publishInfoSubmit(){
-	 var data =$('#publish-form').serializeObject();
-//     console.log(data);
-     var url = baseUrl+'information/publish';
-     $$.ajax({
-         url:url,
-         crossDomain:true,
-         async:false,
-         method:'POST',
-         contentType:'application/json',
-         data:JSON.stringify(data),
-         dataType:'json',
-         success:function (data) {
-             if (data == 1){
-            	 location.href = baseUrl+'wx/index.html';
-             }else{
+function publishInfoSubmit() {
+	var data = $('#publish-form').serializeObject();
+	// console.log(data);
+	var url = baseUrl + 'information/publish';
+	$$.ajax({
+		url : url,
+		crossDomain : true,
+		async : false,
+		method : 'POST',
+		contentType : 'application/json',
+		data : JSON.stringify(data),
+		dataType : 'json',
+		success : function(data) {
+			if (data == 1) {
+				location.href = baseUrl + 'wx/index.html';
+			} else {
 
-             }
-         },
-         error:function (data) {
+			}
+		},
+		error : function(data) {
 
-         }
+		}
 
-     });
+	});
 }
 
-//Loading flag
+// Loading flag
 var loading = false;
 
 /**
  * 初始化上拉加载
  */
-function initPullLoading(){
-    var preHtml = "<div class='infinite-scroll-preloader'>"+
-        "<div class='preloader'></div>"+
-        "</div>";
+function initPullLoading() {
+	var preHtml = "<div class='infinite-scroll-preloader'>"
+			+ "<div class='preloader'></div>" + "</div>";
 
-    $$('.page-content').append($$(preHtml).hide());
+	$$('.page-content').append($$(preHtml).hide());
 
-    // Attach 'infinite' event handler
-    $$('.infinite-scroll').on('infinite', function () {
-        console.log('上拉刷新');
-        $$('.infinite-scroll-preloader').show();
-        // Exit, if loading in progress
-        if (loading) return;
+	// Attach 'infinite' event handler
+	$$('.infinite-scroll').on('infinite', function() {
+		console.log('上拉刷新');
+		$$('.infinite-scroll-preloader').show();
+		// Exit, if loading in progress
+		if (loading)
+			return;
 
-        // Set loading flag
-        loading = true;
+		// Set loading flag
+		loading = true;
 
-        console.log("curr:"+currentPage+",pageNo:"+pageNo);
+		console.log("curr:" + currentPage + ",pageNo:" + pageNo);
 
-        if(currentPage > pageNo){
-            // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
-           myApp.detachInfiniteScroll($$('.infinite-scroll'));
-            // Remove preloader
-            $$('.infinite-scroll-preloader').hide();
-            loading = false;
-            return;
-        }
+		if (currentPage > pageNo) {
+			// Nothing more to load, detach infinite scroll events to prevent
+			// unnecessary loadings
+			myApp.detachInfiniteScroll($$('.infinite-scroll'));
+			// Remove preloader
+			$$('.infinite-scroll-preloader').hide();
+			loading = false;
+			return;
+		}
 
-        getListInfos(infoType,orderType,currentPage);
+		getListInfos(infoType, orderType, currentPage);
 
-        loading = false;
+		loading = false;
 
-    });
-   // myApp.attachInfiniteScroll($$('.infinite-scroll'));
+	});
+	// myApp.attachInfiniteScroll($$('.infinite-scroll'));
 }
 
 /**
@@ -228,19 +234,23 @@ function initPullLoading(){
  * @returns {Number}
  */
 function parsePublishTime(publishTime) {
-    var now = Date.parse(new Date());
-    var s = parseInt((now-publishTime)/1000);
-    if(s < 60)return s+'秒';
-    var m = parseInt(s/60);
-    if(m<60)return m+'分钟';
-    var h = parseInt(m/60);
-    if(h<24)return h+'小时';
-    var d = parseInt(h/24);
-    if(d<30)return d+'天';
-    var mo = parseInt(mo/30);
-    if(mo<12)return mo+'月';
+	var now = Date.parse(new Date());
+	var s = parseInt((now - publishTime) / 1000);
+	if (s < 60)
+		return s + '秒';
+	var m = parseInt(s / 60);
+	if (m < 60)
+		return m + '分钟';
+	var h = parseInt(m / 60);
+	if (h < 24)
+		return h + '小时';
+	var d = parseInt(h / 24);
+	if (d < 30)
+		return d + '天';
+	var mo = parseInt(mo / 30);
+	if (mo < 12)
+		return mo + '月';
 }
-
 
 /**
  * 加载信息列表
@@ -248,63 +258,65 @@ function parsePublishTime(publishTime) {
  * @param data
  */
 function appendInfoList(data) {
-	
-    if (data==null || data == undefined)return;
 
-    $$.each(data.results,function(i,info){
-    	console.log(info);
-    	  var preHtml =   "<li>"+
-          "<div class='item-link item-content'>"+
-              "<div class='item-media avatar'>" +
-                  "<img  src='./img/0.jpg'>" +
-              "</div>"+
-              "<div class='item-inner'>"+
-                  "<div class='item-title-row'>"+
-                     "<div class='item-title'>START_POS→DESTINATION</div>"+
-                      "<div class='item-after publish-time'>PUBLISH_TIME前发布</div>"+
-                  "</div>"+
-                  "<div class='item-subtitle start-time'>出发日期：STARAT_TIME</div>"+
-                  "<div class='item-text'>" +
-                       "<span class='category-type'>CATEGORY</span><span class='contact'>CONTACT</span>" +
-                  "</div>"+
-              "</div>"+
-          "</div>"+
-      "</li>";
-        preHtml = preHtml.replace('START_POS',info.startpos)
-        .replace('DESTINATION',info.destination)
-        .replace('PUBLISH_TIME',parsePublishTime(info.publish_time))
-        .replace('STARAT_TIME',getFormatDateByLong(info.start_time,'MM月dd日hh:mm'))
-        .replace('category-type',info.type==0?'category-youshunche':'category-dashunche')
-        .replace('CATEGORY',info.type==0?'有顺车':'搭顺车')
-        .replace('CONTACT',info.type==0?info.contact+'(车主)':info.contact+'(乘客)');
-        $$('.info-list').append($$(preHtml));
+	if (data == null || data == undefined)
+		return;
 
-    });
+	$$.each(data.results, function(i, info) {
+		// console.log(info);
+		var preHtml = "<li><a href='info-view.html'>"
+				+ "<div class='item-link item-content'>"
+				+ "<div class='item-media avatar'>"
+				+ "<img  src='./img/0.jpg'>"
+				+ "</div>"
+				+ "<div class='item-inner'>"
+				+ "<div class='item-title-row'>"
+				+ "<div class='item-title' style='color: #1a1a1a'>START_POS→DESTINATION</div>"
+				+ "<div class='item-after publish-time'>PUBLISH_TIME前发布</div>"
+				+ "</div>"
+				+ "<div class='item-subtitle start-time'>出发日期：STARAT_TIME</div>"
+				+ "<div class='item-text'>"
+				+ "<span class='category-type'>CATEGORY</span><span class='contact'>CONTACT</span>"
+				+ "</div>" + "</div>" + "</div>" + "</a></li>";
+		preHtml = preHtml.replace('START_POS', info.startpos)
+				.replace('DESTINATION', info.destination)
+				.replace('PUBLISH_TIME',
+						parsePublishTime(info.publish_time))
+				.replace(
+						'STARAT_TIME',
+						getFormatDateByLong(info.start_time,
+								'MM月dd日hh:mm')).replace(
+						'category-type',
+						info.type == 0 ? 'category-youshunche'
+								: 'category-dashunche')
+				.replace('CATEGORY',
+						info.type == 0 ? '有顺车' : '搭顺车')
+				.replace(
+						'CONTACT',
+						info.type == 0 ? info.contact + '(车主)'
+								: info.contact + '(乘客)');
+		$$('.info-list').append($$(preHtml));
+	});
 
-  
-    
-    
-    pageNo = data.pageNo;
-    currentPage = ''+(1+data.currentPage);
-
-
+	pageNo = data.pageNo;
+	currentPage = '' + (1 + data.currentPage);
 
 }
 
 /**
  * 显示list.html
  */
-function showListTab(){
-	$$.get('list.html',{},function (data) {
-	    $$('#list-tab').html(data);
-	    infoType = '0';
-	    orderType = '1';
-	    currentPage = '1';
-	    getListInfos(infoType,orderType,currentPage);
-	    initPullLoading();
+function showListTab() {
+	$$('.subnavbar').show();
+	$$.get('list.html', {}, function(data) {
+		$$('#list-tab').html(data);
+		infoType = '0';
+		orderType = '1';
+		currentPage = '1';
+		getListInfos(infoType, orderType, currentPage);
+		initPullLoading();
 	});
 }
-
 
 /**
  * 获取列表信息
@@ -316,60 +328,64 @@ function showListTab(){
  * @param page
  *            请求页码
  * @param start_pos
- *             出发地点
+ *            出发地点
  * @param destination
- *              终点
+ *            终点
  */
-function getListInfos(type,order,page,start_pos,destination) {
-    // console.log('请求信息列表：'+type+','+order+','+page);
-    var url,data;
-    if(start_pos == null || destination == null){
-        url = baseUrl+'information/get_list';
-        data = {
-            "type":type,
-            "order":order,
-            "page":page
-        };
-    }else{
-        url = baseUrl +'information/search';
-        data = {
-            "type":type,
-            "order":order,
-            "page":page,
-            "start_pos":start_pos,
-            "destination":destination
-        };
-    }
+function getListInfos(type, order, page, start_pos, destination) {
+	// console.log('请求信息列表：'+type+','+order+','+page);
+	var url, data;
+	if (start_pos == null || destination == null) {
+		url = baseUrl + 'information/get_list';
+		data = {
+			"type" : type,
+			"order" : order,
+			"page" : page
+		};
+	} else {
+		url = baseUrl + 'information/search';
+		data = {
+			"type" : type,
+			"order" : order,
+			"page" : page,
+			"start_pos" : start_pos,
+			"destination" : destination
+		};
+	}
 
-    $$.ajax({
-        url:url,
-        crossDomain:true,
-        async:false,
-        method:'POST',
-        contentType:'application/json',
-        data:JSON.stringify(data),
-        dataType:'json',
-        success:function (data) {
-        	console.log(data);
-            appendInfoList(data);
-        },
-        error:function (data) {
+	$$.ajax({
+		url : url,
+		crossDomain : true,
+		async : false,
+		method : 'POST',
+		contentType : 'application/json',
+		data : JSON.stringify(data),
+		dataType : 'json',
+		success : function(data) {
+			console.log(data);
+			appendInfoList(data);
+		},
+		error : function(data) {
 
-        }
+		}
 
-    });
+	});
 }
 
-function parseSex(sex){
-	switch(sex){
-	case "0": return "保密";
-	case "1": return "男";
-	case "2": return "女";
+function parseSex(sex) {
+	switch (sex) {
+	case "0":
+		return "保密";
+	case "1":
+		return "男";
+	case "2":
+		return "女";
 	}
 }
 
-function showPersonalInfo(){
-	//personal information load
+function showPersonalInfo() {
+	$$('.subnavbar').hide();
+	// personal information load
 	$$.ajax({
 		async : false,
 		cache : false,
@@ -385,14 +401,14 @@ function showPersonalInfo(){
 		},
 		success : function(data) {
 			var info = data;
-			$$("#headimgurl").attr("src",info.headimgurl);
+			$$("#headimgurl").attr("src", info.headimgurl);
 			$$("#nickname").html(info.nickname);
 			$$("#sex").html(parseSex(info.sex));
-			$$("#address").html(info.province+","+info.city);
+			$$("#address").html(info.province + "," + info.city);
 			$$("#name").html(info.name);
 			$$("#phone").html(info.phone);
-			
-			function init_personal(){
+
+			function init_personal() {
 				myApp.popup('.popup-personal-info');
 				$$("#li_nickname").hide();
 				$$("#li_sex").hide();
@@ -408,30 +424,31 @@ function showPersonalInfo(){
 				$$("#city_edit").val(info.city);
 				$$("#name_edit").val(info.name);
 				$$("#phone_edit").val(info.phone);
-			};
-			
-			$$("#href_nickname").on('click',function(){
+			}
+			;
+
+			$$("#href_nickname").on('click', function() {
 				init_personal();
 				$$("#li_nickname").show();
 			});
-			$$("#href_sex").on('click',function(){
+			$$("#href_sex").on('click', function() {
 				init_personal();
 				$$("#li_sex").show();
 			});
-			$$("#href_address").on('click',function(){
+			$$("#href_address").on('click', function() {
 				init_personal();
 				$$("#li_address").show();
 			});
-			$$("#href_name").on('click',function(){
+			$$("#href_name").on('click', function() {
 				init_personal();
 				$$("#li_name").show();
 			});
-			$$("#href_phone").on('click',function(){
+			$$("#href_phone").on('click', function() {
 				init_personal();
 				$$("#li_phone").show();
 			});
-			
-			$$('.popup-personal-info').on('close', function () {
+
+			$$('.popup-personal-info').on('close', function() {
 				var storedData = myApp.formToJSON("#personal-form");
 				$$.ajax({
 					async : false,
@@ -446,10 +463,10 @@ function showPersonalInfo(){
 					url : "http://localhost:8080/travel/customer/edit",
 					success : function(data) {
 						info = data;
-						$$("#headimgurl").attr("src",data.headimgurl);
+						$$("#headimgurl").attr("src", data.headimgurl);
 						$$("#nickname").html(data.nickname);
 						$$("#sex").html(parseSex(data.sex));
-						$$("#address").html(data.province+","+data.city);
+						$$("#address").html(data.province + "," + data.city);
 						$$("#name").html(data.name);
 						$$("#phone").html(data.phone);
 						init_personal();
@@ -459,6 +476,3 @@ function showPersonalInfo(){
 		}
 	});
 }
-
-
-
